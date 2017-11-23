395c57dfe92370473e529beecb87199ae83227e2
huangjun <hj644473762@gmail.com>
2017-11-23 16:11:04 +0800

    remove TimerTip

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import ChatInput from './chatinput';
 import MessageList from './messagelist';
-import TimerTip from './TimerTip';
-
 module.exports = {
-    TimerTip: TimerTip,
 	ChatInput: ChatInput,
 	MessageList: MessageList
 };
